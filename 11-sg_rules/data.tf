a5ec7f811562bff901e5bfbd0945b7a284be757c
ManojKoneyagari <koneyagarimanojkumar@gmail.com>
2026-06-25 21:29:35 +0530

    vpc

--- 11-sg_rules/data.tf
+++ 11-sg_rules/data.tf
@@ -1,53 +1,53 @@
 data "aws_ssm_parameter" "mongodb_sg" {
-  name = "mongodb/${var.project}/${var.environment}/sg_id"
+  name = "/mongodb/${var.project}/${var.environment}/sg_id"
 }
 
 data "aws_ssm_parameter" "redis_sg" {
-  name = "redis/${var.project}/${var.environment}/sg_id"
+  name = "/redis/${var.project}/${var.environment}/sg_id"
 }
 
 data "aws_ssm_parameter" "mysql_sg" {
-  name = "mysql/${var.project}/${var.environment}/sg_id"
+  name = "/mysql/${var.project}/${var.environment}/sg_id"
 }
 
 data "aws_ssm_parameter" "rabbitmq_sg" {
-  name = "rabbitmq/${var.project}/${var.environment}/sg_id"
+  name = "/rabbitmq/${var.project}/${var.environment}/sg_id"
 }
 
 data "aws_ssm_parameter" "catalogue_sg" {
-  name = "catalogue/${var.project}/${var.environment}/sg_id"
+  name = "/catalogue/${var.project}/${var.environment}/sg_id"
 }
 
 data "aws_ssm_parameter" "user_sg" {
-  name = "user/${var.project}/${var.environment}/sg_id"
+  name = "/user/${var.project}/${var.environment}/sg_id"
 }
 
 data "aws_ssm_parameter" "cart_sg" {
-  name = "cart/${var.project}/${var.environment}/sg_id"
+  name = "/cart/${var.project}/${var.environment}/sg_id"
 }
 
 data "aws_ssm_parameter" "shipping_sg" {
-  name = "shipping/${var.project}/${var.environment}/sg_id"
+  name = "/shipping/${var.project}/${var.environment}/sg_id"
 }
 
 data "aws_ssm_parameter" "payment_sg" {
-  name = "payment/${var.project}/${var.environment}/sg_id"
+  name = "/payment/${var.project}/${var.environment}/sg_id"
 }
 
 data "aws_ssm_parameter" "backend_alb_sg" {
-  name = "backend_alb/${var.project}/${var.environment}/sg_id"
+  name = "/backend_alb/${var.project}/${var.environment}/sg_id"
 }
 
 data "aws_ssm_parameter" "frontend_sg" {
-  name = "cart/${var.project}/${var.environment}/sg_id"
+  name = "/cart/${var.project}/${var.environment}/sg_id"
 }
 
 data "aws_ssm_parameter" "frontend_alb_sg" {
-  name = "frontend_alb/${var.project}/${var.environment}/sg_id"
+  name = "/frontend_alb/${var.project}/${var.environment}/sg_id"
 }
 
-data "aws_ssm_parameter" "Baston_sg" {
-  name = "Baston/${var.project}/${var.environment}/sg_id"
+data "aws_ssm_parameter" "Bastion_sg" {
+  name = "/Bastion/${var.project}/${var.environment}/sg_id"
 }
 
 data "http" "my_public_ip" {
--- 11-sg_rules/locals.tf
+++ 11-sg_rules/locals.tf
@@ -1,18 +1,18 @@
 locals {
 
-    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg
-    redis_sg_id = data.aws_ssm_parameter.redis_sg
-    mysql_sg_id = data.aws_ssm_parameter.mysql_sg
-    rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg
-    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg
-    user_sg_id = data.aws_ssm_parameter.user_sg
-    cart_sg_id = data.aws_ssm_parameter.cart_sg
-    shipping_sg_id = data.aws_ssm_parameter.shipping_sg
-    payment_sg_id = data.aws_ssm_parameter.payment_sg
-    backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg
-    frontend_sg_id = data.aws_ssm_parameter.frontend_sg
-    frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg
-    bastion_sg_id = data.aws_ssm_parameter.Baston_sg
+    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg.value
+    redis_sg_id = data.aws_ssm_parameter.redis_sg.value
+    mysql_sg_id = data.aws_ssm_parameter.mysql_sg.value
+    rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg.value
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg.value
+    user_sg_id = data.aws_ssm_parameter.user_sg.value
+    cart_sg_id = data.aws_ssm_parameter.cart_sg.value
+    shipping_sg_id = data.aws_ssm_parameter.shipping_sg.value
+    payment_sg_id = data.aws_ssm_parameter.payment_sg.value
+    backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg.value
+    frontend_sg_id = data.aws_ssm_parameter.frontend_sg.value
+    frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg.value
+    bastion_sg_id = data.aws_ssm_parameter.Bastion_sg.value
 
 
 }

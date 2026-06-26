d1bb96a04f5fc70be76378803a09458871a18f91
ManojKoneyagari <koneyagarimanojkumar@gmail.com>
2026-06-26 20:22:41 +0530

    vpc

--- databases/locals.tf
+++ databases/locals.tf
@@ -4,7 +4,7 @@ locals {
   vpc_id           = data.aws_ssm_parameter.vpc_id.value
 
 
-  database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids)[0]
+  database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
 
 
   mongodb_sg_id    = data.aws_ssm_parameter.mongodb_sg.value

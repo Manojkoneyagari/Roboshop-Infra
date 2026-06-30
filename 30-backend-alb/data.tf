89563ca1e87b15beb5b919916348886338d53551
ManojKoneyagari <koneyagarimanojkumar@gmail.com>
2026-06-30 14:39:22 +0530

    renam the  folders

--- 30-backend-alb/data.tf
+++ 30-backend-alb/data.tf
@@ -8,6 +8,9 @@ data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project}/${var.environment}/private_subnet_ids"
 }
 
+data "aws_ssm_parameter" "backend_alb_sg" {
+  name = "/backend_alb/${var.project}/${var.environment}/sg_id"
+}
 data "aws_route53_zone" "backend" {
   name         = "manojkoney.store" # Replace with your registered domain name
   private_zone = false
--- 30-backend-alb/locals.tf
+++ 30-backend-alb/locals.tf
@@ -1,10 +1,9 @@
 locals {
 
-  ami_id           = data.aws_ami.myami.id
+  ami_id           = data.aws_ssm_parameter.vpc_id
   vpc_id           = data.aws_ssm_parameter.vpc_id.value
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
-  bastion_sg_id    = data.aws_ssm_parameter.bastion_sg.value
-  backend_alb-sg-id= data.aws.aws_ssm_parameter.backend_sg.value
+  backend_alb-sg-id= data.aws_ssm_parameter.backend_alb_sg
 
 
   common_tags = {

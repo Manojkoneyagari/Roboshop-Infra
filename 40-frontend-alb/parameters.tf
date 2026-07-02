38f28c0ff67d4030909f00984a53734270225d22
ManojKoneyagari <koneyagarimanojkumar@gmail.com>
2026-07-02 12:22:56 +0530

    renam the  folders

--- 40-frontend-alb/parameters.tf
+++ 40-frontend-alb/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "frontend_alb_listener" {
-  name        = "${var.project}/${var.environment}/frontend_alb_listener"
+  name        = "/${var.project}/${var.environment}/frontend_alb_listener"
   description = " storing frontend_alb_listener arn in ssm"
   type        = "String"
   value       = aws_lb_listener.http.arn
